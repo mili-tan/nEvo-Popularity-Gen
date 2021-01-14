ca145ea2ad8727d464d79a0410711296e8d15ba3
Milkey Tan <819774989@qq.com>
2021-01-14 22:00:45 +0800

    Parallel & sth

--- mPopularity/Program.cs
+++ mPopularity/Program.cs
@@ -14,6 +14,7 @@ namespace mFamousDomain
         static void Main()
         {
             var tasks = new List<Task>();
+            if (!Directory.Exists("source")) Directory.CreateDirectory("source");
             if (!Directory.Exists("result")) Directory.CreateDirectory("result");
             if (!Directory.Exists("temp")) Directory.CreateDirectory("temp");
 
@@ -22,9 +23,10 @@ namespace mFamousDomain
                 try
                 {
                     Console.WriteLine("umbrella...");
-                    new WebClient().DownloadFile("http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip", "umbrella.top-1m.csv.zip");
-                    ZipFile.Open("umbrella.top-1m.csv.zip", ZipArchiveMode.Read).GetEntry("top-1m.csv")
-                        .ExtractToFile("./result/ciscoumbrella.top-1m.csv", true);
+                    new WebClient().DownloadFile("http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip",
+                        "./temp/umbrella.top-1m.csv.zip");
+                    ZipFile.Open("./temp/umbrella.top-1m.csv.zip", ZipArchiveMode.Read).GetEntry("top-1m.csv")
+                        .ExtractToFile("./source/ciscoumbrella.top-1m.csv", true);
                     Console.WriteLine("umbrella ok");
                     GC.Collect();
                 }
@@ -39,9 +41,9 @@ namespace mFamousDomain
                 try
                 {
                     Console.WriteLine("alexa...");
-                    new WebClient().DownloadFile("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip", "alexa.top-1m.csv.zip");
-                    ZipFile.Open("alexa.top-1m.csv.zip", ZipArchiveMode.Read).GetEntry("top-1m.csv")
-                        .ExtractToFile("./result/alexa.top-1m.csv", true);
+                    new WebClient().DownloadFile("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip", "./temp/alexa.top-1m.csv.zip");
+                    ZipFile.Open("./temp/alexa.top-1m.csv.zip", ZipArchiveMode.Read).GetEntry("top-1m.csv")
+                        .ExtractToFile("./source/alexa.top-1m.csv", true);
                     Console.WriteLine("alexa ok");
                     GC.Collect();
                 }
@@ -56,9 +58,9 @@ namespace mFamousDomain
                 try
                 {
                     Console.WriteLine("tranco...");
-                    new WebClient().DownloadFile("https://tranco-list.s3.amazonaws.com/top-1m.csv.zip", "tranco.top-1m.csv.zip");
-                    ZipFile.Open("tranco.top-1m.csv.zip", ZipArchiveMode.Read).GetEntry("top-1m.csv")
-                        .ExtractToFile("./result/tranco.top-1m.csv", true);
+                    new WebClient().DownloadFile("https://tranco-list.s3.amazonaws.com/top-1m.csv.zip", "./temp/tranco.top-1m.csv.zip");
+                    ZipFile.Open("./temp/tranco.top-1m.csv.zip", ZipArchiveMode.Read).GetEntry("top-1m.csv")
+                        .ExtractToFile("./source/tranco.top-1m.csv", true);
                     Console.WriteLine("tranco ok");
                     GC.Collect();
                 }
@@ -74,9 +76,9 @@ namespace mFamousDomain
                 {
                     Console.WriteLine("domcop...");
                     new WebClient().DownloadFile("https://www.domcop.com/files/top/top10milliondomains.csv.zip",
-                        "domcop.top-10m.csv.zip");
+                        "./temp/domcop.top-10m.csv.zip");
                     Console.WriteLine("domcop processing...");
-                    ZipFile.Open("domcop.top-10m.csv.zip", ZipArchiveMode.Read).GetEntry("top10milliondomains.csv")
+                    ZipFile.Open("./temp/domcop.top-10m.csv.zip", ZipArchiveMode.Read).GetEntry("top10milliondomains.csv")
                         .ExtractToFile("./temp/domcop.top10milliondomains.csv", true);
                     var texts = new List<string>();
                     using (var stream = new StreamReader("./temp/domcop.top10milliondomains.csv"))
@@ -87,7 +89,7 @@ namespace mFamousDomain
                         .Select(i => string.Join(',', i[0], i[1])).ToList();
 
                     GC.Collect();
-                    File.WriteAllLines("./result/domcop.top-1m.csv", list);
+                    File.WriteAllLines("./source/domcop.top-1m.csv", list);
                     File.Delete("./temp/domcop.top10milliondomains.csv");
                     Console.WriteLine("domcop ok");
                     GC.Collect();
@@ -108,7 +110,7 @@ namespace mFamousDomain
                     var list = texts.Select(item => item.Replace("\"", string.Empty).Split(',').ToList())
                         .Select(i => string.Join(',', i[0], i[2])).ToList();
 
-                    File.WriteAllLines("./result/majestic.top-1m.csv", list);
+                    File.WriteAllLines("./source/majestic.top-1m.csv", list);
                     File.Delete("./temp/majestic.csv");
                     Console.WriteLine("majestic ok");
                     GC.Collect();
@@ -129,7 +131,7 @@ namespace mFamousDomain
                     var list = texts.Select(item => item.Replace("\"", string.Empty).Split(',').ToList())
                         .Select(i => string.Join(',', i[0], i[1])).ToList();
 
-                    File.WriteAllLines("./result/moz.top-500.csv", list);
+                    File.WriteAllLines("./source/moz.top-500.csv", list);
                     File.Delete("./temp/moz.csv");
                     Console.WriteLine("moz ok");
                     GC.Collect();
@@ -144,10 +146,11 @@ namespace mFamousDomain
 
             try
             {
-                File.Delete("umbrella.top-1m.csv.zip");
-                File.Delete("alexa.top-1m.csv.zip");
-                File.Delete("tranco.top-1m.csv.zip");
-                File.Delete("domcop.top-10m.csv.zip");
+                File.Delete("./temp/umbrella.top-1m.csv.zip");
+                File.Delete("./temp/alexa.top-1m.csv.zip");
+                File.Delete("./temp/tranco.top-1m.csv.zip");
+                File.Delete("./temp/domcop.top-10m.csv.zip");
+                Directory.Delete("./temp/", true);
             }
             catch (Exception e)
             {
@@ -155,23 +158,26 @@ namespace mFamousDomain
             }
 
             GC.Collect();
+            Console.WriteLine("DL DONE!");
 
             var resultDict = new Dictionary<string, int>();
-            foreach (var item in Directory.GetFiles("./result"))
+            foreach (var item in Directory.GetFiles("./source"))
             {
                 foreach (var iStr in File.ReadAllLines(item).ToList().Select(i => i.Split(',')))
-                {
                     resultDict.TryAdd(iStr[1], Convert.ToInt32(iStr[0]));
-                }
                 GC.Collect();
             }
 
-            var resultOrderBy = resultDict.OrderBy(o => o.Value).ToDictionary(o => o.Key, p => p.Value);
-            File.WriteAllLines("./result-origin.csv", resultOrderBy.Select(item => $"{item.Value},{item.Key}").ToList());
             GC.Collect();
+            Console.WriteLine("Read DONE!");
+
+            var resultOrderBy = resultDict.OrderBy(o => o.Value).ToDictionary(o => o.Key, p => p.Value);
+            File.WriteAllLines("./result/result-origin.csv", resultOrderBy.Select(item => $"{item.Value},{item.Key}").ToList());
+            GC.Collect();
+            Console.WriteLine("Select DONE!");
 
             var resultDict2 = new Dictionary<string, int>();
-            foreach (var item in resultOrderBy)
+            Parallel.ForEach(resultOrderBy, item =>
             {
                 var domainName = DomainName.Parse(item.Key);
                 var name = DomainName.Parse(item.Key);
@@ -182,17 +188,19 @@ namespace mFamousDomain
                     if (resultOrderBy.Keys.Contains(name.ToString().TrimEnd('.'))) b = false;
                 }
 
+                // ReSharper disable once AccessToModifiedClosure
                 if (b) resultDict2.TryAdd(domainName.ToString().TrimEnd('.'), item.Value);
-            }
+            });
 
             GC.Collect();
+            Console.WriteLine("Result DONE!");
             resultDict2 = resultDict2.OrderBy(o => o.Value).ToDictionary(o => o.Key, p => p.Value);
             var resultList2 = resultDict2.Select(item => $"{item.Value},{item.Key}").ToList();
-            File.WriteAllLines("./result.csv", resultList2);
-            File.WriteAllLines("./result-1m.csv", resultList2.SkipLast(resultList2.Count - 1000000).ToList());
-            File.WriteAllLines("./result-100k.csv", resultList2.SkipLast(resultList2.Count - 100000).ToList());
-            File.WriteAllLines("./result-10k.csv", resultList2.SkipLast(resultList2.Count - 10000).ToList());
-            File.WriteAllLines("./result-10k.txt",
+            File.WriteAllLines("./result/result.csv", resultList2);
+            File.WriteAllLines("./result/result-1m.csv", resultList2.SkipLast(resultList2.Count - 1000000).ToList());
+            File.WriteAllLines("./result/result-100k.csv", resultList2.SkipLast(resultList2.Count - 100000).ToList());
+            File.WriteAllLines("./result/result-10k.csv", resultList2.SkipLast(resultList2.Count - 10000).ToList());
+            File.WriteAllLines("./result/result-10k.txt",
                 resultDict2.SkipLast(resultDict2.Count - 10000).ToDictionary(o => o.Key, p => p.Value).Keys.ToList());
         }
     }
