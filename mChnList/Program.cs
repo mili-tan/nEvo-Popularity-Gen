dd9dbb2e7e556b5f3072c6949c036edf18105034
Milkey Tan <819774989@qq.com>
2021-01-15 20:34:02 +0800

    sth

--- mChnList/Program.cs
+++ mChnList/Program.cs
@@ -17,42 +17,59 @@ namespace mChnList
         {
             if (!File.Exists("GeoLite2-Country.mmdb"))
                 new WebClient().DownloadFile(
-                    "https://ghproxy.com/github.com/mili-tan/maxmind-geoip/releases/latest/download/GeoLite2-Country.mmdb",
+                    "https://github.com/mili-tan/maxmind-geoip/releases/latest/download/GeoLite2-Country.mmdb",
                     "GeoLite2-Country.mmdb");
             if (!File.Exists("result-10k.csv"))
                 new WebClient().DownloadFile(
-                    "https://ghproxy.com/raw.githubusercontent.com/NovaXNS/popularity/master/result/result-10k.csv",
+                    "https://raw.githubusercontent.com/NovaXNS/popularity/master/result/result-10k.csv",
                     "result-10k.csv");
 
             var countryReader = new DatabaseReader("GeoLite2-Country.mmdb");
             var lines = File.ReadAllLines("result-10k.csv");
             var list = new List<string>();
+            var dnsList = new List<IPAddress>()
+            {
+                IPAddress.Parse("8.8.8.8"), IPAddress.Parse("8.8.4.4"), IPAddress.Parse("1.0.0.1"),
+                IPAddress.Parse("208.67.222.222"), IPAddress.Parse("185.121.177.177"), IPAddress.Parse("9.9.9.9"),
+                IPAddress.Parse("119.29.29.29"), IPAddress.Parse("223.5.5.5"), IPAddress.Parse("180.76.76.76"),
+                IPAddress.Parse("185.222.222.222"), IPAddress.Parse("114.114.114.114"), IPAddress.Parse("117.50.10.10")
+            };
             Parallel.ForEach(lines, item =>
             {
                 try
                 {
-                    ARecord rRecord = null;
-                    int count = 0;
-                    while (rRecord == null)
+                    IPAddress ip = null;
+                    var count = 0;
+                    var name = DomainName.Parse(item.Split(',')[1]);
+                    while (ip == null)
                     {
                         try
                         {
-                            rRecord = new DnsClient(IPAddress.Parse("119.29.29.29"), 1500)
-                                .Resolve(DomainName.Parse(item.Split(',')[1]))
-                                .AnswerRecords.FirstOrDefault() as ARecord;
+                            ip = (new DnsClient(dnsList[new Random(DateTime.Now.Second).Next(dnsList.Count)], 1500)
+                                .Resolve(name).AnswerRecords.FirstOrDefault() as ARecord)?.Address;
                         }
                         catch (Exception)
                         {
-                            count += 1;
-                            Console.WriteLine($"ERR:{count}:{item}");
-                            rRecord = null;
-                            Thread.Sleep(1000);
-                            if (count >= 6) break;
+                            try
+                            {
+                                Console.WriteLine($"HTTP:{count}:{item}");
+                                ip = IPAddress.Parse(new WebClient()
+                                    .DownloadString("http://119.29.29.29/d?dn=" + name.ToString().TrimEnd('.'))
+                                    .Split(';').FirstOrDefault());
+                            }
+                            catch (Exception)
+                            {
+                                count += 1;
+                                ip = null;
+                                Console.WriteLine($"ERR:{count}:{item}");
+                                Thread.Sleep(1500);
+                                if (count >= 3) break;
+                            }
                         }
                     }
 
-                    if (rRecord == null) return;
-                    var country = countryReader.Country(rRecord.Address).Country;
+                    if (ip == null) return;
+                    var country = countryReader.Country(ip).Country;
                     Console.WriteLine(item + "," + country.IsoCode);
                     if (country.IsoCode == "CN") list.Add(item);
                 }
